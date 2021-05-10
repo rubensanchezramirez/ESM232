66c6215ba976a045589be7387d36812a7b43747d
rubensanchezramirez <rubensanchezramirez@ucsb.edu>
2021-05-09 20:52:32 -0700

    update

--- assignments/R/forest_growth.R
+++ assignments/R/forest_growth.R
@@ -1,6 +1,7 @@
 #'  Model of forest growth, where forest size is measured in units of carbon, C
 #' @param time  period of growth
 #' @param C initial carbon
+#' @param parms - list with three values, K, thresh, r, g, and K
 #' @param parms$K - carrying capacity
 #' @param parms$thresh - threshold canopy closure
 #' @param parms$r - base growth rate
@@ -10,22 +11,13 @@
 #'
 forest_growth = function(time, C, parms) {
 
-  # compute rate of change of carbon
-  forest = parms$r*C
-
-  # forest growth is 0 if C is greater than or equal to carrying capacity (K)
-  if(parms$K <= C ) {
-    forest = 0
-
-    # forest growth is linear when carbon is greater than or equal to threshold canopy closure
-  } else if(parms$thresh <= C){
-    forest = parms$g
-
-    # Forest growth is exponential if carbon is below the canopy closure threshold
-  } else {
-    forest = forest
-  }
+  # Forest growth is exponential if carbon is below the canopy closure threshold
+  # Forest growth is linear when carbon is greater than or equal to threshold canopy closure
+  forest = ifelse(C < parms$thresh, parms$r*C, parms$g)
+  # Forest growth is 0 if C is greater than or equal to carrying capacity (K)
+  forest = ifelse(C >= parms$K, 0, forest)
 
   # Return a list of the forest size at time t
   return(list(forest))
+
 }
